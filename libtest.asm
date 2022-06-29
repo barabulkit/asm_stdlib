c05cb75ce938e9d1116f0bb7cd552ea0c8c2d13f
bararbulkit <densolo777@gmail.com>
2022-06-29 15:44:31 +0300

    added print_newline function

--- libtest.asm
+++ libtest.asm
@@ -9,12 +9,15 @@ _start:
 
 mov rdi, 102
 call print_unsigned_int
+call print_newline
 
 mov rdi, -102
 call print_signed_int
+call print_newline
 
 mov rdi, simple_str
 call print_char
+call print_newline
 
 mov rdi, 0
 call exit
